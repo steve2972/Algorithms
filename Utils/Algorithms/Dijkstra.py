2b5bf3b56bfc550c5874083b376c4873afdcbd1a
Steven <steve2972@gmail.com>
2023-04-05 17:04:11 +0900

    Dijkstra w/ heapq

--- Utils/Algorithms/Dijkstra.py
+++ Utils/Algorithms/Dijkstra.py
@@ -10,7 +10,7 @@ class Graph:
         self.edges[u][v] = weight
         self.edges[v][u] = weight
 
-def dijkstra(graph, start):
+def dijkstra_graph(graph, start):
     D = {v:float('inf') for v in range(graph.v)}
     D[start] = 0
 
@@ -32,3 +32,51 @@ def dijkstra(graph, start):
                         D[neighbor] = new_cost
     return D
 
+
+
+from collections import defaultdict
+from heapq import heappop, heappush
+
+def dijkstra(edges, f, t):
+    g = defaultdict(list)
+    for l,r,c in edges:
+        g[l].append((c,r))
+
+    q, seen, mins = [(0,f)], set(), {f: 0}
+    while q:
+        (cost,v1) = heappop(q)
+        if v1 not in seen:
+            seen.add(v1)
+            if v1 == t: return cost
+
+            for c, v2 in g.get(v1, ()):
+                if v2 in seen: continue
+                prev = mins.get(v2, None)
+                next_node = cost + c
+                if prev is None or next_node < prev:
+                    mins[v2] = next_node
+                    heappush(q, (next_node, v2))
+
+    return float("inf")
+
+if __name__ == "__main__":
+    edges = [
+        ("A", "B", 7),
+        ("A", "D", 5),
+        ("B", "C", 8),
+        ("B", "D", 9),
+        ("B", "E", 7),
+        ("C", "E", 5),
+        ("D", "E", 15),
+        ("D", "F", 6),
+        ("E", "F", 8),
+        ("E", "G", 9),
+        ("F", "G", 11)
+    ]
+
+    print("=== Dijkstra ===")
+    print(edges)
+    print("A -> E:")
+    print(dijkstra(edges, "A", "E"))
+    print("F -> G:")
+    print(dijkstra(edges, "F", "G"))
